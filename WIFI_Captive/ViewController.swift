3893e53252d899f4de3e5840c5c4f6f384d2b55c
Sgmedical <sgmedical@sgs-macbook-air.local>
2016-12-16 16:51:40 +0800

    Demo : to get SSID and BSSID . Important notice: the program is not working in Simulator.

--- WIFI_Captive/ViewController.swift
+++ WIFI_Captive/ViewController.swift
@@ -7,12 +7,15 @@
 //
 
 import UIKit
+import SystemConfiguration.CaptiveNetwork
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        printRetrieveWifiNetwork()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +23,33 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func printRetrieveWifiNetwork(){
+        
+        let interfaces = CNCopySupportedInterfaces()! as CFArray
+        if interfaces == nil {
+            return
+        }
+        
+        print("interfaces: \(interfaces)")
+        
+        for i in 0..<CFArrayGetCount(interfaces) {
+            guard let interfaceName: UnsafeRawPointer = CFArrayGetValueAtIndex(interfaces, i) else {
+                return
+            }
+            
+            //let rec = unsafeBitCast(interfaceName, to: AnyObject.self)
+            guard let unsafeInterfaceData = CNCopyCurrentNetworkInfo("\(interfaceName)" as CFString) else {
+                return
+            }
+            
+            guard let interfaceData = unsafeInterfaceData as Dictionary! else {
+                return
+            }
+            
+            print("SSID: \(interfaceData["SSID"]) as! String")
+            print("BSSID: \(interfaceData["BSSID"]) as! String")
+            
+        }
+    }
 }
 
